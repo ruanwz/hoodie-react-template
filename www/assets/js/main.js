48aed431b5148b4fc1036e27edb0514b8662d2eb
David Ruan <ruanwz@gmail.com>
2014-07-01 16:53:59 +0800

    tutorial

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,30 +1,28 @@
 // initialize Hoodie
 var hoodie  = new Hoodie()
-
-// initial load of all todo items from the store
-hoodie.store.findAll('todo').then( function(todos) {
-  todos.sort( sortByCreatedAt ).forEach( addTodo )
-})
-
-// when a new todo gets stored, add it to the UI
-hoodie.store.on('add:todo', addTodo)
-// clear todo list when the get wiped from store
-hoodie.account.on('signout', clearTodos)
-
-// handle creating a new task
-$('#todoinput').on('keypress', function(event) {
-  if (event.keyCode == 13) { // ENTER
-    hoodie.store.add('todo', {title: event.target.value});
-    event.target.value = '';
-  }
-})
-
-function addTodo( todo ) { 
-  $('#todolist').append('<li>'+todo.title+'</li>');
-}
-function clearTodos() {
-  $('#todolist').html('');
-}
-function sortByCreatedAt(a, b) { 
-  return a.createdAt > b.createdAt
+$(document).ready(function () {
+  $("#registerForm").submit(function(e){
+    e.preventDefault();
+    var username = $("#username").val();
+    var password = $("#password").val();
+    hoodie.account.signUp(username, password).done(function (user) {
+        checkuser();
+    });
+  });
+  $("#signout").click(function () {
+      hoodie.account.signOut().done(function (user) {
+          checkuser();
+      });
+  });
+});
+function checkuser() {
+    if(hoodie.account.username){
+        $("#registrationPage").hide();
+        $("#userPage").show();
+        $("#userPage h2").text("Welcome, "+hoodie.account.username+"!");
+    }else{
+        $("#registrationPage").show();
+        $("#userPage").hide();
+        $("#userPage h2").text("");
+    }
 }
